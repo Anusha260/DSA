a92bd2c8e2fa72e9093b3563d9c2346697ff9225
Anusha260 <83698844+Anusha260@users.noreply.github.com>
2022-02-15 16:29:02 +0530

    Add files via upload

---
+++ insertion.py
@@ -0,0 +1,12 @@
+def insertion_sort(arr):
+    for i in range(1,len(arr)):
+        value=arr[i]
+        j=i-1
+        while j>=0 and value<arr[j]:
+            arr[j+1]=arr[j]
+            j-=1
+        arr[j+1]=value
+    return arr
+arr=list(map(int,input("enter the elements:").split()))
+
+print("sorted list1:",insertion_sort(arr))
