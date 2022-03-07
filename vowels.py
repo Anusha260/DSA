4d257b53b00a188188ecbd938e10597740d25437
Anusha260 <83698844+Anusha260@users.noreply.github.com>
2022-03-07 16:18:22 +0530

    Add files via upload

--- vowels.py
+++ vowels.py
@@ -10,8 +10,8 @@ for i in range(0, len(str)):
         vowels = vowels + 1
     elif ((str[i] >= 'a' and str[i] <= 'z')):
         consonants = consonants + 1
-    elif (str[i] ==" " or str[i]=="!" or str[i]=="@" or str[i]=="$"):
-        characterstics = characterstics + 1
+    else:
+        characterstics=characterstics+1
 print("Vowels :", vowels);
 print('Consonants: ', consonants)
 print("characterstics: ",characterstics)
