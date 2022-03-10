a2566ff487ce4a88bd122bcb82b58b6f8374c26c
Anusha260 <83698844+Anusha260@users.noreply.github.com>
2022-03-10 18:48:59 +0530

    Add files via upload

---
+++ reverselist.py
@@ -0,0 +1,43 @@
+
+class Node:
+     
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+ 
+class LinkedList:
+    def __init__(self):
+        self.head = None
+ 
+    def reverse(self):
+        prev = None
+        current = self.head
+        while(current is not None):
+            next = current.next
+            current.next = prev
+            prev = current
+            current = next
+        self.head = prev
+ 
+    def push(self, new_data):
+        new_node = Node(new_data)
+        new_node.next = self.head
+        self.head = new_node
+ 
+    def printList(self):
+        temp = self.head
+        while(temp):
+            print (temp.data,end=" ")
+            temp = temp.next
+ 
+list = LinkedList()
+list.push(26)
+list.push(2)
+list.push(99)
+list.push(4)
+ 
+list.printList()
+list.reverse()
+print ("\nReversed Linked List")
+
+ 
