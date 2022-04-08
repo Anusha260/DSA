6a3d96518613ffb1d5add555a93167463b89ff4a
Anusha260 <83698844+Anusha260@users.noreply.github.com>
2022-04-08 11:41:10 +0530

    Add files via upload

---
+++ diagonaldifference.py
@@ -0,0 +1,19 @@
+N=int(input("enter the number"))
+grid=[]
+for i in range(0,N):
+    list1=[int(i) for i in input("enter the number").split()]
+    grid.append(list1)
+count=0
+sum1=0
+sum2=0
+j1=0
+j2=N-1
+while(count<N):
+    sum1=sum1+grid[count][j1]
+    sum2=sum2+grid[count][j2]
+    count+=1
+    j1+=1
+    j2-=1
+print(sum1-sum2)
+
+
